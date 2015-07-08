b4b54c3a593ea2846232d227ca5327bb23e96765
Liothe <lucy@liothe.se>
2015-07-08 09:59:47 +0200

    preliminary workings of a more 'full' settings

--- ConfigHandler.py
+++ ConfigHandler.py
@@ -1,8 +1,14 @@
+import os
 import json
 
 class Settings():
+    @staticmethod
     def get():
-        config_file = open("settings.json")
+        config_file = open(os.path.abspath(os.path.join(os.path.dirname(__file__), "settings.json")))
         config = json.loads(config_file.read())
         config_file.close()
         return config
+
+    @staticmethod
+    def ie():
+        print(os.path.abspath(os.path.join(os.path.dirname(__file__), "settings.json")))
